31df4830ba3179228604aee9235f34872402f6de
Hema-venkat3 <102861381+Hema-venkat3@users.noreply.github.com>
2023-03-28 20:32:07 +0530

    Update format.java

--- format.java
+++ format.java
@@ -1,5 +1,6 @@
 public class format {
     public static void main(String args[]) {
+        //printing the statements in a specified format
         System.out.println("		HELLO	");
         System.out.println("	WELCOME		TO");
         System.out.println("EDUBRIDGE	 ONLINE		CLASSES");
