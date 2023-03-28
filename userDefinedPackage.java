a7b070c00274ff34043cca39f18eb5ed6f5b546d
Hema-venkat3 <102861381+Hema-venkat3@users.noreply.github.com>
2023-03-28 20:29:31 +0530

    Add files via upload

---
+++ userDefinedPackage.java
@@ -0,0 +1,7 @@
+package myPackage;
+
+public class userDefinedPackage {
+    public static void main(String args[]) {
+        System.out.println("This is my first user defined package program");
+    }
+}
