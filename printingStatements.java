6706199eb1b35841984b207bfa6be9304929795a
Hema-venkat3 <102861381+Hema-venkat3@users.noreply.github.com>
2023-03-28 20:42:22 +0530

    Update printingStatements.java

--- printingStatements.java
+++ printingStatements.java
@@ -1,8 +1,14 @@
 public class printingStatements {
 	public static void main(String args[]) {
+		
+		//declaring and initializing variables 
+		
 		String mother = "KOMATHY", father = "VENKATESAN", address = "NO.18, TOWNHALL 3RD STREET, STUARTPET ", city = "ARAKKONAM", state = "TAMILNADU", country = "INDIA";
 		int age = 21;
 		int contact_number = 923456781;
+		
+		//calling a variables with display message
+		
 		System.out.println("Mother: " + mother);
 		System.out.println("Father: " + father);
 		System.out.println("Age: " + age);
