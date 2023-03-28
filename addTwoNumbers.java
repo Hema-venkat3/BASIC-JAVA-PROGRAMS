6e02cd43b99581d2de9c43d1d5af453d49b21a70
Hema-venkat3 <102861381+Hema-venkat3@users.noreply.github.com>
2023-03-28 20:31:16 +0530

    Update addTwoNumbers.java

--- addTwoNumbers.java
+++ addTwoNumbers.java
@@ -1,9 +1,13 @@
 public class addTwoNumbers {
     public static void main(String args[]) {
+       //DECLARING VARIABLES
         int num1, num2, num3;
+        //initializing values for the variables.
         num1 = 250;
         num2 = 300;
+        //adding two numbers
         num3 = num1 + num2;
+        //displaying the result stored in the variable
         System.out.println("THE ADDITION OF TWO NUMBERS IS :" + num3);
     }
 }
